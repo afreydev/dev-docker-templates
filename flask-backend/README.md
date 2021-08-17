02150692bf6b1547cd989fbbb9b11f7d2c7d2bb8
Angel Rey <afreydev@gmail.com>
2021-08-17 10:39:14 -0500

    updated readme and cleaning stuff

--- flask-backend/README.md
+++ flask-backend/README.md
@@ -4,7 +4,7 @@
 # Start the Flask app (Open localhost:5000/companies/)
 docker-compose up -d
 # Populate the DB
-docker-compose run --rm -ti -v ${PWD}:/opt/src -w /opt/src mysql bash
+docker-compose run --rm -v ${PWD}:/opt/src -w /opt/src mysql bash
 # In the container shell. The password is password D:
 mysql -ucompanies -hmysql -p
 mysql -ucompanies -hmysql -p companies < ./db/creation.sql
--- flask-backend/podman-deploy.sh
+++ flask-backend/podman-deploy.sh
@@ -5,6 +5,13 @@ PODMAN_NETWORK="companies_network"
 DB_CONTAINER_NAME="companies_mariadb"
 FLASK_CONTAINER_NAME="companies"
 
+# Cleaning things
+# podman stop companies_mariadb
+# podman rm companies_mariadb
+# podman stop companies
+# podman rm companies
+# podman network rm companies_network
+
 podman network create "$PODMAN_NETWORK"
 podman run -d --restart=always --env-file=.env --network="$PODMAN_NETWORK" --name="$DB_CONTAINER_NAME" mariadb
 DB_HOST=$(podman inspect --format "{{ .NetworkSettings.Networks.$PODMAN_NETWORK.IPAddress }}" $DB_CONTAINER_NAME)
